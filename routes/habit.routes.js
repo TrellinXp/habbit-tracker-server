5303980b4fd50f488fee1fb81a061c354464d6d5
claradrojas <drc.clara@gmail.com>
2021-08-26 12:36:54 +0200

    habit get update and delete routes

--- routes/habit.routes.js
+++ routes/habit.routes.js
@@ -26,4 +26,51 @@ router.post('/habits', (req, res, next) => {
       .catch(err => res.json(err));
 });
 
+//GET route-- to get an specific detail view
+router.get('/habits/:habitId', (req, res, next) => {
+    const { habitId } = req.params;
+  
+    if (!mongoose.Types.ObjectId.isValid(habitId)) {
+      res.status(400).json({ message: 'Specified id is not valid' });
+      return;
+    }
+  
+    // Our projects have array of tasks' ids and
+    // we can use .populate() method to get the whole task objects
+    Habit.findById(habitId)
+      .populate('user')
+      .then(habit => res.status(200).json(habit))
+      .catch(error => res.json(error));
+  });
+
+  //Update habit
+  router.put('/habits/:habitId', (req, res, next) => {
+    const { habitId } = req.params;
+    const {title, description, amount, unit, date, user} = req.body
+  
+    if (!mongoose.Types.ObjectId.isValid(habitId)) {
+      res.status(400).json({ message: 'Specified id is not valid' });
+      return;
+    }
+  
+    Habit.findByIdAndUpdate(habitId, {title, description, amount, unit, date, user} )
+      .then(() => res.json({ message: `Habit with ${habitId} is updated successfully.` }))
+      .catch(error => res.json(error));
+  });
+
+  router.delete('/habits/:habitId', (req, res, next) => {
+    const { habitId } = req.params;
+    if (!mongoose.Types.ObjectId.isValid(habitId)) {
+      res.status(400).json({ message: 'Specified id is not valid' });
+      return;
+    }
+
+    //TO DO validation for user deleting project
+  
+    Habit.findByIdAndRemove(habitId)
+      .then(() => res.json({ message: `Habit with ${habitId} is removed successfully.` }))
+      .catch(error => res.json(error));
+  });
+
+
 module.exports = router
