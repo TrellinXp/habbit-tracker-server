17fc89074dde58d5c8f152a135d585218c1adce4
Peter Mayer <peter_my@gmx.de>
2021-09-02 08:59:49 +0200

    require session

--- config/index.js
+++ config/index.js
@@ -1,23 +1,19 @@
 // We reuse this import in order to have access to the `body` property in requests
 const express = require("express");
-
 // ℹ️ Responsible for the messages you see in the terminal as requests are coming in
 // https://www.npmjs.com/package/morgan
 const logger = require("morgan");
-
 // ℹ️ Needed when we deal with cookies (we will when dealing with authentication)
 // https://www.npmjs.com/package/cookie-parser
 const cookieParser = require("cookie-parser");
-
 // ℹ️ Serves a custom favicon on each request
 // https://www.npmjs.com/package/serve-favicon
 const favicon = require("serve-favicon");
-
 // ℹ️ global package used to `normalize` paths amongst different operating systems
 // https://www.npmjs.com/package/path
 const path = require("path");
-
 const cors = require("cors");
+const session = require('express-session');
 
 // Middleware configuration
 module.exports = (app) => {
